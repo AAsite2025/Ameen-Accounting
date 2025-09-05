348dde1d6e62d7014db2cd24a3812b699b70c385
AAsite2025 <ntyhalatyhala@gmail.com>
2025-09-05 16:44:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -53,16 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
-  document.addEventListener("DOMContentLoaded", function () {
-    const collapsibles = document.querySelectorAll(".collapsible");
-    collapsibles.forEach(button => {
-      button.addEventListener("click", function () {
-        this.classList.toggle("active");
-        const content = this.nextElementSibling;
-        content.style.display = content.style.display === "block" ? "none" : "block";
-      });
-    });
-  });
+
 
 
 
